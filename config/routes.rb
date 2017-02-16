762a12e03f83e62388f842e70e42c024b36ea00b
David Kim <davidjinuk90@gmail.com>
2017-02-16 20:05:28 +0000

    Generated model and fixed migration issue

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,6 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
     resources :categories, only: [:index, :create, :show, :new]
   end
-  #my code
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
