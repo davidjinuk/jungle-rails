371083f8194d9846da7a87e48ccc087a7326f627
David Kim <davidjinuk90@gmail.com>
2017-02-17 21:28:17 +0000

    User gets email after the order is placed.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,6 +11,8 @@ class OrdersController < ApplicationController
     order  = create_order(charge)
 
     if order.valid?
+      #send email after order is placed
+      UserMailer.order_email(order).deliver_later
       empty_cart!
       redirect_to order, notice: 'Your Order has been placed.'
     else
---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,9 @@
+class UserMailer < ApplicationMailer
+  default from: 'no-reply@jungle.com'
+
+  def order_email(order)
+    @order = order
+    # @url  = 'http://example.com/login'
+    mail(to: @order.email, subject: @order.id)
+  end
+end
