72bccc32837bef3c87fb856bba1c246d0ba6cdee
David Kim <davidjinuk90@gmail.com>
2017-02-27 23:03:07 +0000

    Created average review for each product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,8 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find params[:id]
     @review = Review.new
+    @reviews = Review.where(product_id: @product.id)
+    @product_rating = @reviews.average(:rating)
   end
 
 end
